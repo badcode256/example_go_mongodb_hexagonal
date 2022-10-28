7f018bda09a7ac3248ac5f9c4567d7b1a46ea82b
badcode256 <badcode256@gmail.com>
2022-10-28 15:27:35 -0500

    FEAT carga de proyectoc v2

--- cmd/bootstrap/bootstrap.go
+++ cmd/bootstrap/bootstrap.go
@@ -11,15 +11,9 @@ import (
 	"github.com/badcode256/example_go_mongodb_hexagonal/internal/service"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
-	//_ "github.com/denisenkom/go-mssqldb"
 )
 
-//ZcxyWxJm5OBzu336
-//mongodb+srv://user_business:<password>@cluster0.dvbglbv.mongodb.net/?retryWrites=true&w=majority
-//mongodb+srv://user_business:<password>@cluster0.dvbglbv.mongodb.net/test
-//mongodb+srv://user_business:ZcxyWxJm5OBzu336@cluster0.dvbglbv.mongodb.net/dbBusiness?retryWrites=true\&w=majority
-
-var uriDb = "mongodb+srv://user_business:ZcxyWxJm5OBzu336@cluster0.dvbglbv.mongodb.net/dbBusiness?retryWrites=true&w=majority"
+var uriDb = "mongodb+srv://user:pass@cluster0.dvbglbv.mongodb.net/dbBusiness?retryWrites=true&w=majority"
 
 func Start() error {
 
@@ -35,13 +29,6 @@ func Start() error {
 		log.Fatal(err.Error())
 		return err
 	}
-
-	/*mySqlURI := fmt.Sprintf("server=%s;user id=%s;password=%s;port=1433;database=Business;", os.Getenv("SQL_SERVER"), os.Getenv("SQL_USER"), os.Getenv("SQL_PASSWORD"))
-	db, err := sql.Open("sqlserver", mySqlURI)
-
-	if err != nil {
-		return err
-	}*/
 
 	userRepository := user.NewUserRepository(db)
 	userService := service.NewUserService(userRepository)
